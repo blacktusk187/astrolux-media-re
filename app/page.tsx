a5157e6a15c9d31953a36fdfef7e924fa56b5744
Tim Barnsley <barnsleyt@gmail.com>
2025-02-11 16:03:26 -0800

    Add Vercel Analytics and Speed Insights to improve performance tracking

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import Portfolio from "./components/Portfolio"
 import Prices from "./components/Prices"
 import Contact from "./components/Contact"
 import Footer from "./components/Footer"
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
 export default function Home() {
   return (
