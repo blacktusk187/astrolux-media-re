f310ac3356b0b767cf400116bc3229275bb7a10b
Tim Barnsley <barnsleyt@gmail.com>
2025-02-17 11:05:20 -0800

    Update image URL for Websites service and add hostname to next.config

--- app/components/Services.tsx
+++ app/components/Services.tsx
@@ -39,7 +39,7 @@ const services = [
     title: "Websites",
     description: "Custom-designed websites to showcase your properties and services online",
     image:
-        "https://images.unsplash.com/photo-1460925895917-afdab827c52f?q=80&w=2015&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "https://www.realestatewebmasters.com/blog/image-optimization-and-seo-for-photos/",
   },
 ]
 
