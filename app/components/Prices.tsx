04daa82686160ad7108edcf33b3656f0ec59d46e
Tim Barnsley <barnsleyt@gmail.com>
2025-02-18 15:32:58 -0800

    Update Walk-Thru Videos image format from webp to webm for improved compatibility

--- app/components/Prices.tsx
+++ app/components/Prices.tsx
@@ -49,7 +49,7 @@ const additionalServices: AdditionalService[] = [
   {
     title: "Walk-Thru Videos",
     description: "Immersive property tours",
-    image: "https://res.cloudinary.com/dkzt44dkk/image/upload/v1739906752/im4mraodw0chplknepps.webp",
+    image: "https://res.cloudinary.com/dkzt44dkk/video/upload/v1739921521/ztuoolie4fce35pnvfyb.webm",
     options: [
       { name: "60 Second Instagram Video", details: "no music", price: "$449" },
       { name: "2-3 minute full Video", details: "with music", price: "$599" },
