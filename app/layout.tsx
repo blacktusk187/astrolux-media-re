385d36b1e7129d550e15e19d32c7894a8d5ef6c2
Tim Barnsley <barnsleyt@gmail.com>
2025-02-21 10:04:11 -0800

    Remove unused preload link for image in layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,11 +28,6 @@ export default function RootLayout({
             <title>{metadata.title}</title>
             <link rel="icon" href="/favicon.ico" sizes="any" />
             <link rel="apple-touch-icon" href="/apple-icon.png" />
-            <link
-                rel="preload"
-                href="https://res.cloudinary.com/dkzt44dkk/image/upload/f_auto,q_auto,w_auto,c_scale,dpr_auto/v1740027413/m4bqq0vnrt8rknqlj7cj.webp"
-                as="image"
-            />
         </head>
         <body className={`font-sans ${lexend.variable} ${montserrat.variable} ${jura.variable}`}>
         <Navbar />
